6d7164862680dc54b299e01012c9a965ad8e10be
ntwari bruce <ntwaribruce647@gmail.com>
2023-02-24 01:01:54 +0200

    Final features on user side

--- project_code/views.py
+++ project_code/views.py
@@ -10,3 +10,28 @@ views = Blueprint('views', __name__)
 @login_required
 def index():
     return render_template("index.html",user=current_user)
+
+@views.route('/buy', methods=['GET', 'POST'])
+@login_required
+def buy():
+    return render_template("buy.html", user=current_user)
+
+@views.route('/rent', methods=['GET', 'POST'])
+@login_required
+def rent():
+    return render_template("rent.html", user=current_user)
+
+@views.route('/favourite', methods=['GET', 'POST'])
+@login_required
+def favoutire():
+    return render_template("favourite.html", user=current_user)
+
+@views.route('/appointment', methods=['GET', 'POST'])
+@login_required
+def appointment():
+    return render_template("appointment.html", user=current_user)
+
+@views.route('/view_property', methods=['GET', 'POST'])
+@login_required
+def view_property():
+    return render_template("view_property.html", user=current_user)
